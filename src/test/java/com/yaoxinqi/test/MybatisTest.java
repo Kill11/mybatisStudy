4abbbaa0129e841fa91fa1985f94dd03cabc0bd2
Kill11 <415340367@qq.com>
2020-03-02 22:21:23 +0800

    20200302:看了源码后我想错了，mybatis的数据类不需要遵守javabean约定，所以他不是用内省类获取方法设定值的，而是直接用反射的方式设定值的

--- src/test/java/com/yaoxinqi/test/MybatisTest.java
+++ src/test/java/com/yaoxinqi/test/MybatisTest.java
@@ -74,17 +74,22 @@ public class MybatisTest {
     因为mybatis查完之后只知道数据库的字段名
     所以是用数据库的字段名来获得PropertyDescriptor的
     这就说明了为什么对象的属性名必须和数据库字段名一致
+
+    update:
+        上面不对，mybatis是用反射直接设置属性值，所以不需要get、set方法
+        不过你没有指定字段与属性的映射时，mybatis用的是简单映射，就直接字段是什么，属性就是什么，这个时候是要保持一致的
+        不过你也可以指定字段与属性的映射关系，那就是mybatis的复杂映射了
      */
-    @Test
-    public void test4() throws Exception{
-        User user = new User();
-        PropertyDescriptor pd = new PropertyDescriptor("id",User.class);
-        System.out.println(pd.getWriteMethod());
-        Method writeMethod = pd.getWriteMethod();//用来写的，执行时告诉他往哪个对象的id写，写什么
-        Method readMethod = pd.getReadMethod();//用来读的，执行时告诉他读哪个对象的id
-        //把获取的列的值，给对象赋值
-        writeMethod.invoke(user,123);
-        System.out.println("直接获取到的："+user.getId());
-        System.out.println("获取到的get方法读到的："+readMethod.invoke(user));
-    }
+//    @Test
+//    public void test4() throws Exception{
+//        User user = new User();
+//        PropertyDescriptor pd = new PropertyDescriptor("id",User.class);
+//        System.out.println(pd.getWriteMethod());
+//        Method writeMethod = pd.getWriteMethod();//用来写的，执行时告诉他往哪个对象的id写，写什么
+//        Method readMethod = pd.getReadMethod();//用来读的，执行时告诉他读哪个对象的id
+//        //把获取的列的值，给对象赋值
+//        writeMethod.invoke(user,123);
+//        System.out.println("直接获取到的："+user.getId());
+//        System.out.println("获取到的get方法读到的："+readMethod.invoke(user));
+//    }
 }
