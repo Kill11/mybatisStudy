a6e58c623accdeff17fe187b1c7b97af78943e78
Kill11 <415340367@qq.com>
2020-03-02 21:13:45 +0800

    20200302:了解了一下mybatis查询完成后封装对象的原理

--- src/test/java/com/yaoxinqi/test/MybatisTest.java
+++ src/test/java/com/yaoxinqi/test/MybatisTest.java
@@ -11,8 +11,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.testng.annotations.Test;
 
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class MybatisTest {
@@ -65,5 +67,24 @@ public class MybatisTest {
             System.out.println(user);
         }
         in.close();
+    }
+
+    /*
+    看下内省类是怎么获得set、get方法的
+    因为mybatis查完之后只知道数据库的字段名
+    所以是用数据库的字段名来获得PropertyDescriptor的
+    这就说明了为什么对象的属性名必须和数据库字段名一致
+     */
+    @Test
+    public void test4() throws Exception{
+        User user = new User();
+        PropertyDescriptor pd = new PropertyDescriptor("id",User.class);
+        System.out.println(pd.getWriteMethod());
+        Method writeMethod = pd.getWriteMethod();//用来写的，执行时告诉他往哪个对象的id写，写什么
+        Method readMethod = pd.getReadMethod();//用来读的，执行时告诉他读哪个对象的id
+        //把获取的列的值，给对象赋值
+        writeMethod.invoke(user,123);
+        System.out.println("直接获取到的："+user.getId());
+        System.out.println("获取到的get方法读到的："+readMethod.invoke(user));
     }
 }
